eb5999db1fbb55a47a702e0ccf76de81530e6e81
b091527336 <81487783+b091527336@users.noreply.github.com>
2026-09-20 08:30:39 +0800

    feat: add customer and organization routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrganizationController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/dashboard');
@@ -14,7 +16,15 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
     Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
     Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+
+    Route::resource('customers', CustomerController::class)
+        ->only(['index', 'show', 'edit', 'update']);
+
+    Route::resource('organizations', OrganizationController::class)
+        ->only(['index', 'show', 'edit', 'update']);
+
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
