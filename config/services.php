62a65cd0bea6cca1697ddc5975ba00287d26ac61
b091527336 <81487783+b091527336@users.noreply.github.com>
2026-09-21 04:00:07 +0800

    feat: configure TableSit Partner API

--- config/services.php
+++ config/services.php
@@ -1,8 +1,16 @@
 <?php
 
+$tableSitBaseUrl = env('TABLESIT_BASE_URL', 'https://www.tablesit.co/api/v1');
+
 return [
     'tablesit' => [
-        'base_url' => env('TABLESIT_BASE_URL', 'https://www.tablesit.co/api/v1'),
+        'base_url' => $tableSitBaseUrl,
+        'partner_base_url' => env(
+            'TABLESIT_PARTNER_BASE_URL',
+            str_replace('/api/v1', '/api/partner/v1', $tableSitBaseUrl),
+        ),
+        'partner_api_key' => env('TABLESIT_PARTNER_API_KEY'),
+        'owner_email' => env('TABLESIT_OWNER_EMAIL', env('DBP_ADMIN_EMAIL')),
         'api_keys' => json_decode(env('TABLESIT_API_KEYS_JSON', '{}'), true) ?: [],
         'timeout' => (int) env('TABLESIT_TIMEOUT', 15),
         'max_pages' => (int) env('TABLESIT_MAX_PAGES', 100),
