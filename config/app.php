f16d82343fc3ced8d5a2d64a0332542516604826
b091527336 <81487783+b091527336@users.noreply.github.com>
2026-09-20 08:20:41 +0800

    chore: add runtime support for config/app.php

---
+++ config/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'Danshi Booking Platform'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'Asia/Taipei',
+    'locale' => env('APP_LOCALE', 'zh_TW'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'zh_TW'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'zh_TW'),
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => array_filter(explode(',', env('APP_PREVIOUS_KEYS', ''))),
+    'maintenance' => [
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
+        'store' => env('APP_MAINTENANCE_STORE', 'database'),
+    ],
+];
